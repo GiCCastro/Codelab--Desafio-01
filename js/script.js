952ba6b1af5b7fc5605c236d5adff4f30489eee5
GiCCastro <gicristina.scastro@gmail.com>
2025-02-13 17:08:26 -0300

    ajuste e preenchimento no ícone de coração

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function search() {
+    let input = document.getElementById('searchbar').value.toLowerCase();
+    let cardContent = document.getElementsByClassName('card');
+    let noResults = document.getElementById('no-results');
+    let found = false;
+
+    for (let i = 0; i < cardContent.length; i++) {
+        let card = cardContent[i].querySelector('h2');
+        if (input === "") {
+            cardContent[i].style.display = "block";
+            found = true;
+        } else if (card.innerHTML.toLowerCase().includes(input)) {
+            cardContent[i].style.display = "block";
+            found = true;
+        } else {
+            cardContent[i].style.display = "none";
+        }
+    }
+
+    if (!found && input !== "") {
+        noResults.style.display = "flex";
+    } else {
+        noResults.style.display = "none";
+    }
+
+}
+
+const btn = document.querySelectorAll(".card-header__image")
+btn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        btn.classList.toggle("filled")
+    })
+})
